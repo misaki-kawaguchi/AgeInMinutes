9b8e712d321b5ce075ceee554053afbd114b8aed
川口美咲 <misaki@arcphilia.co.jp>
2020-12-29 22:06:46 +0900

    [Add]カレンダーの年・月・日を取得

--- app/src/main/java/com/misakikawaguchi/ageinminutes/MainActivity.kt
+++ app/src/main/java/com/misakikawaguchi/ageinminutes/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +22,19 @@ class MainActivity : AppCompatActivity() {
 
     // Select Dateボタンを押した時の処理
     fun clickDatePicker(view: View) {
+
+        /*
+         *デフォルトのタイムゾーンとロケールを使用してカレンダーを取得（現在の時刻に基づく）
+         */
+
+        // getInstance()を使ってインスタンスを取得
+        val myCalendar = Calendar.getInstance()
+        // 「年」に関する値を取得
+        val year = myCalendar.get(Calendar.YEAR)
+        // 「月」に関する値を取得
+        val month = myCalendar.get(Calendar.MONTH)
+        // 「日」に関する値を取得
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
 
     }
 }
