c0e9a39b2208cf599a1bbfd4e2592963416870b1
川口美咲 <misaki@arcphilia.co.jp>
2020-12-29 21:32:54 +0900

    [Add]Select Dateボタンのidを取得、処理を追加

--- app/src/main/java/com/misakikawaguchi/ageinminutes/MainActivity.kt
+++ app/src/main/java/com/misakikawaguchi/ageinminutes/MainActivity.kt
@@ -2,10 +2,25 @@ package com.misakikawaguchi.ageinminutes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        // 親コンストラクターを呼び出す
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Select Dateボタン(btnDatePicker)のidを取得
+        val btnDatePicker:Button = findViewById(R.id.btnDatePicker)
+        //Select Dateボタンを押す
+        btnDatePicker.setOnClickListener {view ->
+            clickDatePicker(view)
+        }
+    }
+
+    // Select Dateボタンを押した時の処理
+    fun clickDatePicker(view: View) {
+
     }
 }
